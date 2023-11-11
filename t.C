2078ce4b7ca3dd69fe1e864cdb0e1a7856cb7e25
Richard Jones <rjones30@gmail.com>
2023-11-11 11:17:57 -0500

    * add a "test successful" message if t.C completes without crashing [rtj]

--- t.C
+++ t.C
@@ -1,9 +1,11 @@
 #include <dilog.h>
+#include <iostream>
 
 int main() {
    for (int i=0; i < 10; ++i) {
       dilog::block myloop("mytrun", "myloop");
       dilog::get("mytrun").printf("iteration %d\n",i);
    }
+   std::cout << "test successful!" << std::endl;
    return 0;
 }
